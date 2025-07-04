3e3a670faab8f127257ad49225783632c6478fa0
m9stafa05 <m9stafa05@gmail.com>
2025-07-04 18:50:24 +0300

    Implement The Login Cubit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:scholar_chat/constants.dart';
+import 'package:scholar_chat/controllers/cubit/login_cubit.dart';
 import 'package:scholar_chat/helper/show_snack_bar.dart';
 import 'package:scholar_chat/pages/chat_page.dart';
 import 'package:scholar_chat/pages/regester_page.dart';
@@ -9,20 +10,15 @@ import 'package:scholar_chat/widgets/custom_form_textfield.dart';
 import 'package:scholar_chat/widgets/custom_button.dart';
 
 // m9stafa05@gmail.com
+// mer@gmail.com
+//12345678
 // ignore: must_be_immutable
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-  static String id = 'LoginPage';
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+  static const String id = 'LoginPage';
   String? email;
   String? password;
   bool isLoading = false;
-
   GlobalKey<FormState> formKey = GlobalKey();
 
   @override
@@ -45,146 +41,109 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: Color(0xff274460),
       ),
       body: Center(
-        child: ModalProgressHUD(
-          inAsyncCall: isLoading,
-          child: Form(
-            key: formKey,
-            child: ListView(
-              children: [
-                Image(image: AssetImage(kLogo), height: 200),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
+        child: BlocConsumer<LoginCubit, LoginState>(
+          listener: (context, state) {
+            if (state is LoginLoading) {
+              isLoading = true;
+            } else if (state is LoginSuccess) {
+              isLoading = false;
+              Navigator.pushNamed(
+                context,
+                ChatPage.id,
+                arguments: email,
+              );
+            } else if (state is LoginFailure) {
+              isLoading = false;
+              ShowSnackBar(context, message: state.errorMessage);
+            }
+          },
+          builder:
+              (context, state) => ModalProgressHUD(
+                inAsyncCall: isLoading,
+                child: Form(
+                  key: formKey,
+                  child: ListView(
+                    children: [
+                      Image(image: AssetImage(kLogo), height: 200),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                // Email TextField
-                CustomFormTextField(
-                  onChange: (data) {
-                    email = data;
-                  },
-                  labelText: "Enter your email",
-                  hintText: 'Your email',
-                ),
-                SizedBox(height: 20),
-                // Password TextField
-                CustomFormTextField(
-                  onChange: (data) {
-                    password = data;
-                  },
-                  isPassword: true,
-                  labelText: 'Enter your password',
-                  hintText: 'At least 8 characters',
-                ),
-                SizedBox(height: 20),
-                CustomButton(
-                  onTap: () async {
-                    if (formKey.currentState!.validate()) {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      try {
-                        await LogInUser();
-                        ShowSnackBar(
-                          // ignore: use_build_context_synchronously
-                          context,
-                          message: 'Successfully Logged in',
-                          color: Colors.green,
-                        );
-                        Navigator.pushNamed(
-                          // ignore: use_build_context_synchronously
-                          context,
-                          ChatPage.id,
-                          arguments: email,
-                        );
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'user-not-found') {
-                          ShowSnackBar(
-                            // ignore: use_build_context_synchronously
-                            context,
-                            message: 'This email is not registered',
-                          );
-                        } else if (e.code == 'wrong-password') {
-                          ShowSnackBar(
-                            // ignore: use_build_context_synchronously
-                            context,
-                            message: 'Wrong password',
-                          );
-                        } else if (e.code == 'invalid-email') {
-                          ShowSnackBar(
-                            // ignore: use_build_context_synchronously
-                            context,
-                            message: 'Please enter a valid email',
-                          );
-                        } else if (e.code == 'too-many-requests') {
-                          ShowSnackBar(
-                            // ignore: use_build_context_synchronously
-                            context,
-                            message:
-                                'Too many requests. Please try again later.',
-                          );
-                        }
-                      } catch (e) {
-                        ShowSnackBar(
-                          // ignore: use_build_context_synchronously
-                          context,
-                          message: 'Oops! Something went wrong',
-                        );
-                        debugPrint('Error : ${e.toString()}');
-                      }
-                      setState(() {
-                        isLoading = false;
-                      });
-                    } else {}
-                  },
-                  text: 'LOGIN',
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, RegisterPage.id);
-                      },
-                      child: Text(
-                        ' REGISTER',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
+                      SizedBox(height: 20),
+                      // Email TextField
+                      CustomFormTextField(
+                        onChange: (data) {
+                          email = data;
+                        },
+                        labelText: "Enter your email",
+                        hintText: 'Your email',
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 20),
+                      // Password TextField
+                      CustomFormTextField(
+                        onChange: (data) {
+                          password = data;
+                        },
+                        isPassword: true,
+                        labelText: 'Enter your password',
+                        hintText: 'At least 8 characters',
+                      ),
+                      SizedBox(height: 20),
+                      CustomButton(
+                        onTap: () {
+                          if (formKey.currentState!.validate()) {
+                            BlocProvider.of<LoginCubit>(
+                              context,
+                            ).logInUser(
+                              email: email!,
+                              password: password!,
+                            );
+                          } else {}
+                        },
+                        text: 'LOGIN',
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Don\'t have an account?',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                RegisterPage.id,
+                              );
+                            },
+                            child: Text(
+                              ' REGISTER',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ),
+              ),
         ),
       ),
-    );
-  }
-
-  // ignore: non_constant_identifier_names
-  Future<void> LogInUser() async {
-    final auth = FirebaseAuth.instance;
-    UserCredential user = await auth.signInWithEmailAndPassword(
-      email: email ?? 'Enter your email',
-      password: password ?? 'Enter your password',
     );
   }
 }
