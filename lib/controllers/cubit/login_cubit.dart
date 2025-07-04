cd419bafc76424969addbe30cb59f62dc214cb98
m9stafa05 <m9stafa05@gmail.com>
2025-07-04 18:55:14 +0300

    add debugPrint

--- lib/controllers/cubit/login_cubit.dart
+++ lib/controllers/cubit/login_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/widgets.dart';
 
 part 'login_state.dart';
 
@@ -36,9 +36,8 @@ class LoginCubit extends Cubit<LoginState> {
           ),
         );
       } else {
-        emit(
-          LoginFailure(errorMessage: "Unexpected error: ${e.code}"),
-        );
+        emit(LoginFailure(errorMessage: "Unexpected error"));
+        debugPrint(e.code);
       }
     } catch (e) {
       emit(LoginFailure(errorMessage: "Opps SomeThing Went Wrong"));
