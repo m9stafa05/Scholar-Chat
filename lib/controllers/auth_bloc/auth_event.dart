fd0dc3796c07b3b675fd462ba9b3ee9b5bfcade7
m9stafa05 <m9stafa05@gmail.com>
2025-07-05 21:39:11 +0300

    Provide email & password To the Login And Register Event

--- lib/controllers/auth_bloc/auth_event.dart
+++ lib/controllers/auth_bloc/auth_event.dart
@@ -1,7 +1,17 @@
 part of 'auth_bloc.dart';
 
 @immutable
-sealed class AuthEvent {}
+sealed class AuthEvent {
+  const AuthEvent();
+}
 
-class LoginEvent extends AuthEvent {}
-class RegisterEvent extends AuthEvent {}
+class LoginEvent extends AuthEvent {
+  final String email;
+  final String password;
+  const LoginEvent({required this.email, required this.password});
+}
+class RegisterEvent extends AuthEvent {
+  final String email;
+  final String password;
+  const RegisterEvent({required this.email, required this.password});
+}
