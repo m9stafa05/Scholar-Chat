9dfa790e84bfd0dc367b6a4e3af69ee0eda00e5c
m9stafa05 <m9stafa05@gmail.com>
2025-07-05 14:17:30 +0300

    Fix NDK Version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.scholar_chat"
     compileSdk = 35
-    ndkVersion =  "29.0.13113456"
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
